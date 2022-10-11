c37030ea9efa7dbc2392fd007d873461c8db1394
Giorgi Bokuchava <113925720+Boku52500@users.noreply.github.com>
2022-10-12 00:58:23 +0400

    Fix linters

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
 const closeBtn = document.querySelector('.closeButton');
 closeBtn.addEventListener('click', () => {
-  let menuBackground = document.querySelector('#menuBackground');
+  const menuBackground = document.querySelector('#menuBackground');
   menuBackground.classList.add('display-none');
-  let menuContent = document.querySelector('#menuContent');
+  const menuContent = document.querySelector('#menuContent');
   menuContent.classList.add('display-none');
-  let menuButton = document.querySelectorAll('.dropdowns');
+  const menuButton = document.querySelectorAll('.dropdowns');
   menuButton.forEach((item) => {
     item.classList.add('display-none');
-  })
-})
+  });
+});
 
-let hambBtn = document.querySelector('#hamb-button');
+const hambBtn = document.querySelector('#hamb-button');
 hambBtn.addEventListener('click', () => {
-  let menuContent = document.querySelector('#menuContent');
+  const menuContent = document.querySelector('#menuContent');
   menuContent.classList.remove('display-none');
-  let menuBackground = document.querySelector('#menuBackground');
+  const menuBackground = document.querySelector('#menuBackground');
   menuBackground.classList.remove('display-none');
-  let closeButt = document.querySelector('.closeButton');
+  const closeButt = document.querySelector('.closeButton');
   closeButt.classList.remove('display-none');
-  let menuButton = document.querySelectorAll('.dropdowns');
+  const menuButton = document.querySelectorAll('.dropdowns');
   menuButton.forEach((item) => {
     item.classList.remove('display-none');
-  })
-})
+  });
+});
 
-let menuLink = document.querySelectorAll('.dropdowns');
+const menuLink = document.querySelectorAll('.dropdowns');
 
 menuLink.forEach((item) => {
   item.addEventListener('click', () => {
-    let menuBackground = document.querySelector('#menuBackground');
+    const menuBackground = document.querySelector('#menuBackground');
     menuBackground.classList.add('display-none');
-    let menuContent = document.querySelector('#menuContent');
+    const menuContent = document.querySelector('#menuContent');
     menuContent.classList.add('display-none');
-    let menuButton = document.querySelectorAll('.dropdowns');
+    const menuButton = document.querySelectorAll('.dropdowns');
     menuButton.forEach((item) => {
-      item.classList.add('display-none')
-    })
-  })
-})
+      item.classList.add('display-none');
+    });
+  });
+});
